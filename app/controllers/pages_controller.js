4b461ac5109be466ebfbf6cb5af105d46798283e
boekfors <spam@ekfors.eu>
2013-12-12 11:40:26 +0100

    Initial

---
+++ app/controllers/pages_controller.js
@@ -0,0 +1,25 @@
+///<reference path='app.d.ts'/>
+// TypeScript
+var locomotive = require('locomotive'), Controller = locomotive.Controller;
+
+var PagesController = new Controller();
+
+PagesController.main = function () {
+    this.title = 'Ekfors@Home';
+    this.text = "Text som ska visas på sidan";
+    this.render();
+};
+
+PagesController.light = function () {
+    this.title = 'Ekfors@Home';
+    this.text = "Belysning";
+    this.render();
+};
+
+PagesController.heat = function () {
+    this.title = 'Ekfors@Home';
+    this.text = "Värme";
+    this.render();
+};
+
+module.exports = PagesController;
